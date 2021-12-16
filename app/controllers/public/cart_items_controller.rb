ab0d8ee84257ea103a1018f5e2471ccb67ffa270
EC2 Default User <yooyama.97.01@gmail.com>
2021-12-16 15:10:55 +0000

    [Add] 商品一覧画面 ＆ 商品詳細画面 & カート一覧画面 一通り作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,19 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    new_cart_item = CartItem.new(cart_item_params)
+    new_cart_item.customer_id = current_customer.id
+    new_cart_item.item_id = cart_item_params[:item_id]
+    new_cart_item.save
+    redirect_to public_cart_items_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:count, :item_id)
+  end
 end
