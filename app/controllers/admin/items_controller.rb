231122e596fec68100a183e99598785b7a3eba54
masaya0731 <masaya0806543@gmail.com>
2021-12-18 05:27:10 +0000

    [Add] 管理者ログイン,

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,9 +1,36 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
 
+  def index
+    @items = Item.all
+  end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render "admin/items/new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
 
   private
+
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:genre_id, :name, :image, :introduction, :price_without_tax, :is_sold)
   end
+
 end
