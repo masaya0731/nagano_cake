8f885f853052161cb7cb7efc70ac7a2b037d878f
EC2 Default User <yooyama.97.01@gmail.com>
2021-12-18 11:31:38 +0000

    [Update] ログイン画面レイアウト変更

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
     @all_items = Item.all
+    @genres = Genre.all
     @items = Item.page(params[:page]).per(12).reverse_order
   end
 
   def show
     @item = Item.find(params[:id])
+    @genres = Genre.all
     @tax_rate = 1.1
     @new_cart_item = CartItem.new
   end
