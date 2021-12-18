c3f6fa3b917f8f2441222fcbe65b8ec050b11f91
yuka-team <yuaaasly116@gmail.com>
2021-12-18 10:35:23 +0000

    [Add]admin-customers

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,28 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    # kaminariを使用した記述
+    @customers = Customer.page(params[:page])
+  end
+
+  def show
+    # 会員詳細の表示
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    # 会員情報編集
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    # 会員情報の更新
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer.id)
+  end
+
+   private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :last_name_kana, :post_code, :address, :email, :tel, :is_deleted)
+    end
 end
