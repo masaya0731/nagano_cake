530eec1851a1b70968daa49828c29aaf7da4d8dc
yuka-team <yuaaasly116@gmail.com>
2021-12-19 08:58:41 +0000

    [Add]order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,22 @@
 class Admin::OrdersController < ApplicationController
 
   def show
-    # @order = Order.find(params[:id])
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
   end
 
+
   def update
+      order = Order.find(params[:id])
+      if order.update(order_params)
+        redirect_to admin_order_path(order.id)
+      else
+        render admin_order_path
+      end
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:order_status)
+    end
 end
