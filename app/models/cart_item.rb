af9a0dff06ff4ba119da7f01286d81e8a8d92520
EC2 Default User <yooyama.97.01@gmail.com>
2021-12-23 06:20:25 +0000

    [Fix] 買い物を続けるはトップ画面へ、個数選択せずにカート追加不可、各種注文進捗のリンク修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,7 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-  validates :count, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 20}
+  validates :count, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 20}
 
   # 小計を求めるメソッド
   def subtotal_price
